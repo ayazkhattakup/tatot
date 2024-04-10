129c4fd4c6ca771653aba54ddae703223df1375b
ayazkhattakup <ayazkhattakupwork@gmail.com>
2024-04-09 18:14:21 -0700

    'added'

--- Book_Shelves/urls.py
+++ Book_Shelves/urls.py
@@ -27,4 +27,7 @@ urlpatterns = [
 if settings.DEBUG is False:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+if settings.DEBUG is False:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
